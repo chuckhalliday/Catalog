4f4f37ba04b6cb63522c58d023b1ec338d412165
chuckhalliday <charleshallidayclarkii@gmail.com>
2022-06-01 12:57:30 -0700

    Initial commit

---
+++ Controllers/itemsController.cs
@@ -0,0 +1,26 @@
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    // Get /items
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+    }
+}
